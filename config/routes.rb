6eee4ceaad4be6ef2e76ea1048902aa0aa925d1b
kenzo <kenzou.kenzou104809@gmail.com>
2020-08-01 19:02:53 +0900

    利用規約ページを作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'welcome#index'
+  get '/terms', to: 'welcome#terms'
   resources :schedules do
     get :daily, on: :collection
   end
