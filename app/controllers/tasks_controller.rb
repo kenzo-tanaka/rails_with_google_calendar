edff842ea5abdc95788a244cb26ad7a0778d5e7e
kenzo <kenzou.kenzou104809@gmail.com>
2020-08-01 16:29:00 +0900

    今日の予定を取得できるよう修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,6 +8,7 @@ class TasksController < ApplicationController
 
   # リソースを作成するわけではないので命名変更
   # TODO: 日別のタスク取得に変更
+  # @see https://developers.google.com/calendar/quickstart/ruby
   def create
     client = current_user.get_google_calendar_client
     response = client.list_events(
@@ -15,7 +16,8 @@ class TasksController < ApplicationController
       max_results: MAX_RESULTS,
       single_events: true,
       order_by: 'startTime',
-      time_min: Time.zone.now)
+      time_min: Time.zone.today.to_datetime,
+      time_max: Time.zone.tomorrow.to_datetime)
 
     response.items.each do |e|
       start = e.start.date || e.start.date_time
