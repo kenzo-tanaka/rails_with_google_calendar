93dac92bcfa77e04630bfa70c57689a39468d500
kenzo <kenzou.kenzou104809@gmail.com>
2020-08-01 16:23:16 +0900

    イベントを取得する処理を実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,22 @@
-require "google/apis/calendar_v3"
-require "google/api_client/client_secrets.rb"
-
 class TasksController < ApplicationController
   CALENDAR_ID = 'primary'
 
   def index; end
+
+  # リソースを作成するわけではないので命名変更
+  def create
+    client = current_user.get_google_calendar_client
+    response = client.list_events(
+      CALENDAR_ID,
+      max_results: 50,
+      single_events: true,
+      order_by: 'startTime',
+      time_min: Time.zone.now)
+
+    response.items.each do |e|
+      start = e.start.date || e.start.date_time
+      puts "#{e.summary}: #{start}"
+    end
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -17,5 +17,32 @@ class User < ApplicationRecord
       )
     end
     user
-  end  
+  end
+
+  def get_google_calendar_client
+    client = Google::Apis::CalendarV3::CalendarService.new
+    secrets = Google::APIClient::ClientSecrets.new({
+      "web" => {
+        "access_token" => self.access_token,
+        "refresh_token" => self.refresh_token,
+        "client_id" => ENV["GOOGLE_API_KEY"],
+        "client_secret" => ENV["GOOGLE_API_SECRET"]
+      }
+    })
+
+    begin
+      client.authorization = secrets.to_authorization
+      client.authorization.grant_type = "refresh_token"
+
+      client.authorization.refresh!
+      update_attributes(
+        access_token: client.authorization.access_token,
+        refresh_token: client.authorization.refresh_token,
+        expires_at: client.authorization.expires_at.to_i
+      )
+    rescue => e
+      Rails.logger.error e.message
+    end
+    client
+  end
 end
